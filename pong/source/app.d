64a732938a5c991dad9a735c0c6223b79f2e2548
Zachary Davis <67290867+smokeytube@users.noreply.github.com>
2022-04-16 23:00:37 +0300

    Added pong

---
+++ pong/source/app.d
@@ -0,0 +1,230 @@
+import glui;
+import raylib;
+
+import std.array;
+import std.range;
+import std.format;
+import std.stdio;
+import std.conv;
+import std.string;
+
+import core.math;
+
+//Data
+immutable int WIDTH = 858;
+immutable int HEIGHT = 525;
+
+int static_block_size = HEIGHT / 30;
+float xi = WIDTH / 2;
+float yi = HEIGHT / 2;
+int i1 = -1;
+int i2 = 1;
+float x;
+float y;
+bool point_scored = false;
+
+bool set_fps = true;
+int player_y = (HEIGHT / 2) - 50;
+int player2_y = (HEIGHT / 2) - 50;
+
+int paddle_width = 10;
+int paddle_height = 100;
+int projectile_width = 10;
+int projectile_height = 10;
+float projectile_slope = 1;
+float projectile_speed = 2;
+int paddle_speed = 2;
+int mov_speed = 60;
+
+int player_score = 0;
+int player2_score = 0;
+
+void main()
+{
+
+    //Create window
+    InitWindow(WIDTH, HEIGHT, "Pong");
+
+    scope (exit)
+        CloseWindow();
+
+    // --------- User interface start ---------
+    auto theme = makeTheme!q{
+        GluiFrame.styleAdd!q{
+            margin = 10;
+            backgroundColor = color!"#FFFFFF";
+        };
+    };
+
+    auto whiteTheme = makeTheme!q{
+        GluiFrame.styleAdd.backgroundColor = color!"#ffffff";
+        GluiButton!().styleAdd.backgroundColor = Colors.WHITE;
+    };
+
+    GluiSpace root, clearScreen, endScreen;
+
+    clearScreen = vspace(
+            .layout!(1, "fill"),
+            theme,
+            hspace(
+                .layout!(1, "fill"),
+            ),
+    );
+
+    endScreen = hframe(
+        vframe(
+            
+            .layout!(1, "fill"),
+            whiteTheme,
+            hspace(
+            
+            .layout!(1, "fill"),
+            label("Point Scored!"),
+            ),
+    ),
+    );
+
+    root = clearScreen;
+
+    // --------- User interface end ---------
+
+    //Loop which will draw the screensaver and UI while the window is not closed.
+    while (!WindowShouldClose())
+    {
+        if (point_scored)
+        {
+            root = endScreen;
+            for (int i = 0; i < 180; i++)
+            {
+                BeginDrawing();
+                root.draw();
+                EndDrawing();
+            }
+            root = clearScreen;
+            point_scored = false;
+            projectile_speed = 2;
+            xi = WIDTH / 2;
+            yi = HEIGHT / 2;
+        }
+        else
+        {
+            if (set_fps)
+            {
+                SetTargetFPS(mov_speed);
+                set_fps = false;
+            }
+            BeginDrawing();
+            processEvents();
+
+            //This loop draws the different sides of the pong court. The loop starts at -1 opposed to 0 to have the first square not be drawn.
+            for (int i = -1; i < 30; i += 2)
+            {
+                //I looked at an image of pong, and the height was 30 of these squares tall, hence why static_block_size appears often.
+                DrawRectangleV(Vector2(WIDTH / 2 + (HEIGHT / 30) / 4, i * (static_block_size)), Vector2(
+                        static_block_size, static_block_size), Colors
+                        .LIGHTGRAY);
+            }
+            //Draw the top border and bottom border
+            DrawRectangleV(Vector2(0, 0), Vector2(WIDTH, static_block_size), Colors.LIGHTGRAY);
+            DrawRectangleV(Vector2(0, HEIGHT - static_block_size), Vector2(WIDTH, static_block_size), Colors
+                    .LIGHTGRAY);
+
+            //Draw the paddles
+            DrawRectangleV(Vector2(HEIGHT / 15, player_y), Vector2(paddle_width, paddle_height), Colors
+                    .WHITE);
+            DrawRectangleV(Vector2(WIDTH - (HEIGHT / 15), player2_y), Vector2(paddle_width, paddle_height), Colors
+                    .WHITE);
+
+            DrawText(toStringz(to!string(player_score)), WIDTH / 2 - 50, HEIGHT / 30, 50, Colors
+                    .WHITE);
+            DrawText(toStringz(to!string(player2_score)), WIDTH / 2 + 50, HEIGHT / 30, 50, Colors
+                    .WHITE);
+            updateProjectile();
+            root.draw();
+            ClearBackground(Colors.BLACK);
+            EndDrawing();
+        }
+    }
+}
+
+void processEvents()
+{
+    if (IsKeyDown(KeyboardKey.KEY_S) && player_y < HEIGHT - paddle_height - static_block_size)
+    {
+        player_y += paddle_speed;
+    }
+    else if (IsKeyDown(KeyboardKey.KEY_W) && player_y > static_block_size)
+    {
+        player_y -= paddle_speed;
+    }
+    if (IsKeyDown(KeyboardKey.KEY_DOWN) && player2_y < HEIGHT - paddle_height - static_block_size)
+    {
+        player2_y += paddle_speed;
+    }
+    else if (IsKeyDown(KeyboardKey.KEY_UP) && player2_y > static_block_size)
+    {
+        player2_y -= paddle_speed;
+    }
+}
+
+void updateProjectile()
+{
+    xi += projectile_speed * i1;
+    yi += projectile_speed * i2;
+
+    x = xi;
+    y = yi * projectile_slope;
+
+    void onBounce()
+    {
+        //Create a new slope, 0.7 to 1, for a slight variation in projectile movement.
+        float generated_slope = player_y / y;
+        yi = setSlopeAndYCoord(generated_slope, projectile_slope, yi);
+        projectile_slope = generated_slope;
+        projectile_speed += 0.05;
+    }
+
+    //If statements to make the rectangle bounce off the walls.
+    if (x <= (HEIGHT / 15) + paddle_width && y >= player_y && y <= player_y + paddle_height)
+    {
+        onBounce();
+        i1 = 1;
+    }
+    else if (x >= WIDTH - (HEIGHT / 15) - paddle_width && y >= player2_y && y <= player2_y + paddle_height)
+    {
+        onBounce();
+        i1 = -1;
+    }
+    if (y >= HEIGHT - projectile_height - static_block_size || y <= static_block_size)
+    {
+        i2 *= -1;
+    }
+    if (x >= WIDTH - projectile_width)
+    {
+        player_score++;
+        projectile_speed = 0;
+        xi -= 2;
+        point_scored = true;
+    }
+    else if (x < 0)
+    {
+        player2_score++;
+        projectile_speed = 0;
+        xi += 2;
+        point_scored = true;
+    }
+
+    DrawRectangleV(Vector2(x, y), Vector2(projectile_width, projectile_height), Colors.WHITE);
+}
+
+/*This function is needed because otherwise, the rectange jumps unnaturally when the slope is set.
+*/
+float setSlopeAndYCoord(float newslope, float oldslope, float oldyi)
+{
+    float a = (oldyi) * (oldslope);
+    float b = (oldyi) * newslope;
+    //If the new slope is higher, the ratio will be below 1. If the new slope is lower, the ratio will be above 1.
+    float ratio = a / b;
+    //Multiply int yi by the ratio to get the appropriate position for the new slope, so the rectangle doesn't jump.
+    return oldyi * ratio;
+}
---
+++ screensaver/source/app.d
@@ -0,0 +1,217 @@
+import glui;
+import raylib;
+
+import std.array;
+import std.range;
+import std.format;
+import std.stdio;
+import std.conv;
+
+//All data immutable by the user
+immutable int WIDTH = 512;
+immutable int HEIGHT = 512;
+float xi = 0;
+float yi = 0;
+int i1 = 1;
+int i2 = 1;
+int i3 = 1;
+int fnt = 40;
+float x;
+float y;
+const char* text = "DVD";
+bool set_fps = true;
+bool set_slope = true;
+
+//All data mutable by the user
+int rec_width = 150;
+int rec_height = 50;
+float slope = 0.5;
+int mov_speed = 60; //in FPS
+
+void main() {
+
+    //Create window
+	InitWindow(WIDTH, HEIGHT, "Screensaver");
+
+    scope (exit) CloseWindow();
+
+    // --------- User interface start ---------
+    auto theme = makeTheme!q{
+        GluiFrame.styleAdd!q{
+
+            margin = 10;
+            backgroundColor = color!"#fffa";
+        };
+    };
+
+    immutable rightTheme = makeTheme!q{
+        GluiSpace.styleAdd!q{
+            margin = 5;
+            margin.sideY = 10;
+        };
+        GluiButton!().styleAdd!q{
+            margin.sideTop = 10;
+        };
+    };
+
+    GluiScrollBar slopeBar, widthBar, heightBar, speedBar;
+    GluiSpace root, settings, clearScreen;
+
+    settings = vspace(
+        .layout!(1, "fill"),
+        theme,
+
+        vframe(
+            button("Close", { root = clearScreen; }),
+        ),
+        vscrollFrame(
+            .layout!(1, "fill", "start"),
+            rightTheme,
+            vspace(
+                label("Change slope"),
+                slopeBar = hscrollBar(.layout!"fill"),
+
+                button("Save", delegate {
+                    set_slope = true;
+                }),
+            ),
+        ),
+        vscrollFrame(
+            .layout!(1, "fill", "start"),
+            rightTheme,
+            vspace(
+                label("Change width and height"),
+                widthBar = hscrollBar(.layout!"fill"),
+                heightBar = hscrollBar(.layout!"fill"),
+
+                button("Save", {
+                    rec_width = to!int(widthBar.position/10);
+                    rec_height = to!int(heightBar.position/10);
+                    /*Since font fnt corresponds nicely to the height, a little extra calculation
+                    * is needed for thw width to make the text fit in the rectangle*/
+                    if (rec_height+rec_height/1.5 >= rec_width) {
+                        fnt = to!int(rec_width*0.8*0.5);
+                    } else {
+                        fnt = to!int(rec_height*0.8);
+                    }
+                }),
+            ),
+        ),
+        vscrollFrame(
+            .layout!(1, "fill", "start"),
+            rightTheme,
+            vspace(
+                label("Change the movement speed"),
+                speedBar = hscrollBar(.layout!"fill"),
+
+                button("Save", delegate {
+                    //+30 has to be added as to keep a minimum of 30 fps. Otherwise the UI will lag or outright freeze.
+                    mov_speed = to!int(speedBar.position)+30;
+                    set_fps = true;
+                }),
+            ),
+        ),
+    );
+
+    clearScreen = vspace(
+        .layout!(1, "fill"),
+        theme,
+
+        vframe(
+            button("Settings", { root = settings; }),
+        ),
+        hspace(
+            .layout!(1, "fill"),
+        ),
+    );
+
+    slopeBar.availableSpace = 5_000;
+    widthBar.availableSpace = WIDTH*10;
+    heightBar.availableSpace = HEIGHT*10;
+    speedBar.availableSpace = 960;
+
+    slopeBar.position = to!int(slope*1000);
+    widthBar.position = rec_width*10;
+    heightBar.position = rec_height*10;
+    speedBar.position = mov_speed;
+
+    root = clearScreen;
+
+    // --------- User interface end ---------
+
+    //Loop which will draw the screensaver and UI while the window is not closed
+	while (!WindowShouldClose())
+    {
+        if (set_fps) {
+            SetTargetFPS(mov_speed);
+            set_fps = false;
+        }
+        if (set_slope) {
+            //0.001 has to be added because the program will misbehave if the value is 0
+            setSlopeAndYCoord(to!float(slopeBar.position)/1000+0.001, &slope, &yi);
+            set_slope = false;
+        }
+        BeginDrawing();
+        ClearBackground(Colors.BLACK);
+        update();
+        root.draw();
+        EndDrawing();
+	}
+}
+
+void update()
+{
+	xi += i1;
+	yi += i2;
+
+	x = xi;
+	y = yi * slope;
+
+    writeln(x);
+
+    //If statements to make the rectangle bounce off the walls.
+	if (x >= WIDTH - rec_width)
+	{
+		i1 = -1;
+	}
+	else if (x <= 0)
+	{
+		i1 = 1;
+	}
+	if (y >= HEIGHT - rec_height)
+	{
+		i2 = -1;
+	}
+	else if (y <= 0)
+	{
+		i2 = 1;
+	}
+
+	DrawRectangleV(Vector2(x, y), Vector2(rec_width, rec_height), Colors.BLUE);
+
+	/*Draws text in the center of the rectange. It works well enough, although sometimes the text is ever so slightly off center.
+	* That is not my fault. There is no function for drawing a text with vecotors, only an integer X and Y.*/
+	DrawText(text,
+	to!int((x + rec_width / 2) - (to!string(text).length)*(fnt/2.825)), 
+	to!int((y + rec_height / 2) - (to!string(text).length) - (fnt/3)),
+	 fnt, Colors.WHITE);
+}
+
+/*This function is needed because otherwise, the rectange jumps unnaturally when the slope is set.
+* A pointer to the variable slope and yi, defined at the beggining of the program, is passed to this function,
+* so this function can be easily pasted into other programs.
+*/
+void setSlopeAndYCoord(float newslope, float *oldslope, float *oldyi) {
+    float a = (*oldyi) * (*oldslope);
+    float b = (*oldyi) * newslope;
+    //If the new slope is higher, the ratio will be below 1. If the new slope is lower, the ratio will be above 1.
+    float ratio = a/b;
+    // Try-catch block is needed because if the ratio is 0, like when the program starts up, the program will crash.
+    try {
+        //Multiply int yi by the ratio to get the appropriate position for the new slope, so the rectangle doesn't jump.
+        *oldyi = to!int((*oldyi) * ratio);
+    } catch (std.conv.ConvException e) {
+        *oldyi = 0;
+    }
+    *oldslope = newslope;
+}
