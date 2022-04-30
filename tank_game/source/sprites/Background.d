2a79517c27552e3b8e93d3cdfc4d652309ff7116
Zachary Davis <67290867+smokeytube@users.noreply.github.com>
2022-05-01 01:23:42 +0300

    clouds

--- tank_game/source/sprites/Background.d
+++ tank_game/source/sprites/Background.d
@@ -10,9 +10,11 @@ class Background {
     Texture background1;
     Texture background2;
     Texture background3;
+    Texture sky;
     Rectangle background_rect1;
     Rectangle background_rect2;
     Rectangle background_rect3;
+    Rectangle sky_rect1;
     int background_width;
     int background_height;
 
@@ -22,6 +24,7 @@ class Background {
         this.background1 = LoadTexture("./source/sprites/res/background1.png");
         this.background2 = LoadTexture("./source/sprites/res/background2.png");
         this.background3 = LoadTexture("./source/sprites/res/background3.png");
+        this.sky = LoadTexture("./source/sprites/res/sky.png");
         setScale();
     }
 
@@ -29,6 +32,7 @@ class Background {
         this.background_rect1 = Rectangle(x, y, this.background_width, this.background_height);
         this.background_rect2 = Rectangle(x/1.25, y, this.background_width, this.background_height);
         this.background_rect3 = Rectangle(x/2, y, this.background_width, this.background_height);
+        this.sky_rect1 = Rectangle(x/10, y, this.background_width, this.background_height);
         for (int i = 0; i < screen_width; i += background_width) {
             DrawTextureRec(this.background3, this.background_rect3, Vector2(i, screen_height-this.background_height), Colors.WHITE);
         }
@@ -37,6 +41,9 @@ class Background {
         }
         for (int i = 0; i < screen_width; i += background_width) {
             DrawTextureRec(this.background1, this.background_rect1, Vector2(i, screen_height-this.background_height), Colors.WHITE);
+        }
+        for (int i = 0; i < screen_width; i += background_width) {
+            DrawTextureRec(this.sky, this.sky_rect1, Vector2(i, 0), Colors.WHITE);
         }
     }
 
@@ -51,10 +58,14 @@ class Background {
         this.background2.height = this.background_height;
         this.background3.width = this.background_width;
         this.background3.height = this.background_height;
+        this.sky.width = this.background_width;
+        this.sky.height = this.background_height;
     }
 
     ~this() {
         UnloadTexture(this.background1);
         UnloadTexture(this.background2);
+        UnloadTexture(this.background3);
+        UnloadTexture(this.sky);
     }
 }
